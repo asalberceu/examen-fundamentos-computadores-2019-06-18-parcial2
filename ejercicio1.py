14c757faff2edc7d2765f99be5cb27d5cbd38b27
danymarta <m.berruezo2@usp.ceu.es>
2019-06-18 15:44:57 +0200

    Añadido examen Diana Montero

--- ejercicio1.py
+++ ejercicio1.py
@@ -10,6 +10,7 @@ morse = {'A': '.-',     'B': '-...',   'C': '-.-.',
          'Y': '-.--',   'Z': '--..'}
 
 
+
 def codificar_palabra(palabra):
     """
     Función que codifica una palabra en morse.
@@ -18,8 +19,16 @@ def codificar_palabra(palabra):
     Devuelve:
         El código morse correspondiente a la palabra con los bloques de código de cada letra separados por punto y coma.
     """
+    palabra = palabra.upper()
+    palabra_codificada = []
+    for i in palabra:
+        for j,k in morse.items():
+            if i == j:
+                palabra_codificada.append(k)
 
     return palabra_codificada
+
+#print(codificar_palabra("hola"))
 
 
 def decodificar_palabra(palabra):
@@ -30,6 +39,16 @@ def decodificar_palabra(palabra):
     Devuelve:
         La palabra decodificada.
     """
+    
+    nuevo_morse = dict((v, k) for (k, v) in morse.items())
+    #print(nuevo_morse)
+    nueva_palabra = palabra.split(";")
+    palabra_decodificada = []
+
+    for i in nueva_palabra:
+        for clave,valor in nuevo_morse.items():
+            if i == clave:
+                palabra_decodificada.append(valor)
 
     # A partir del diccionario morse construir otro diccionario invertido, es decir, cuyas claves sean los códigos morse y cuyos valores sean las letras asociadas.
 
@@ -37,6 +56,7 @@ def decodificar_palabra(palabra):
 
     return palabra_decodificada
 
+#print(decodificar_palabra("--..;-.--"))
 
 def codificar_mensaje(mensaje):
     """
@@ -46,11 +66,14 @@ def codificar_mensaje(mensaje):
     Devuelve:
         El código morse correspondiente al mensaje con letras separadas por punto y coma y palabras separadas por espacio.
     """
+    mensaje_nuevo = mensaje.split(' ')
+    mensaje_codificado = list(map(codificar_palabra, mensaje_nuevo))
 
     return mensaje_codificado
 
+#print(codificar_mensaje("Hola mundo"))
 
-def decodificar_mensaje(palabra):
+def decodificar_mensaje(mensaje):
     """
     Función que decodifica un mensaje en morse.
     Parámetros:
@@ -58,14 +81,16 @@ def decodificar_mensaje(palabra):
     Devuelve:
         El mensaje decodificado.
     """
+    mensaje_nuevo = mensaje.split(' ')
+    mensaje_decodificado = list(map(decodificar_palabra, mensaje_nuevo))
 
     return mensaje_decodificado
 
 
 
 # Llamada a las funciones de prueba
-print(codificar_palabra('Python')) #Debe devolver '.--.;-.--;-;....;---;-.'
-print(decodificar_palabra('.--.;-.--;-;....;---;-.')) #Debe devolver 'PYTHON'
-print(codificar_mensaje('Hola Python')) #Debe devolver '....;---;.-..;.- .--.;-.--;-;....;---;-.'
-print(decodificar_mensaje('....;---;.-..;.- .--.;-.--;-;....;---;-.')) #Debe devolver 'HOLA PYTHON'
+#print(codificar_palabra('Python')) #Debe devolver '.--.;-.--;-;....;---;-.'
+#print(decodificar_palabra('.--.;-.--;-;....;---;-.')) #Debe devolver 'PYTHON'
+#print(codificar_mensaje('Hola Python')) #Debe devolver '....;---;.-..;.- .--.;-.--;-;....;---;-.'
+#print(decodificar_mensaje('....;---;.-..;.- .--.;-.--;-;....;---;-.')) #Debe devolver 'HOLA PYTHON'
 
--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,5 @@
-def leer_fichero(url):
+def leer_fichero(ruta):
+    #Utilizo la ruta en lugar de la url por que el ordenador no podia decodificar los datos con latin1
     """
     Función que abre un fichero de texto desde una url y devuelve una lista de listas con los datos del fichero.
     Parámetros:
@@ -6,8 +7,16 @@ def leer_fichero(url):
     Devuelve:
         Una lista cuyos elementos son a su vez listas que contienen los datos de cada línea del fichero menos la primera línea.
     """
-
+    f = open(ruta, "r")
+    f.readline()
+    datos = f.readlines()
+    datos_viviendas = []
+    for linea in datos:
+        datos_viviendas.append(linea + ";")
     return datos_viviendas
+
+print(leer_fichero("AdjudicacionAlquilerViviendas.csv"))
+
 
 
 def distritos(datos_viviendas):
@@ -18,8 +27,18 @@ def distritos(datos_viviendas):
     Devuelve:
         Una lista con los distritos correspondientes a cada lista.
     """
-
+    listas = []
+    distritos = []
+    for i in datos_viviendas:
+        listas = datos_viviendas[i]
+    #print(lista)
+        for lista in listas: 
+            distritos = lista[0]
     return distritos
+
+    
+
+
 
 
 def filtrar_distritos(datos_viviendas, distritos):
@@ -31,6 +50,10 @@ def filtrar_distritos(datos_viviendas, distritos):
     Devuelve:
         Una lista con las listas que contienen los datos de las viviendas arrendadas de los distritos indicados.
     """
+    datos_distritos = []
+    for distrito in distritos:
+        if distrito in datos_viviendas:
+            datos_distritos = datos_viviendas[distrito]
 
     return datos_distritos
 
@@ -43,14 +66,19 @@ def viviendas_distritos(datos_viviendas):
     Devuelve:
         Un diccionario cuyas claves son los nombres de los distritos y cuyos valores son el número total de viviendas arrendadas en cada distrito.
     """
+    viviendas = {}
+    for i in datos_viviendas:
+        c = datos_viviendas[0][0]
+        v = datos_viviendas[0][1]
+        viviendas = {c:v}
 
     return viviendas
 
 
 
 # Llamada a las funciones de prueba
-datos = leer_fichero('https://datos.madrid.es/egob/catalogo/300117-0-arrendamiento-programas.csv')
-print(distritos(datos))
-datos_filtrados = filtrar_distritos(datos, ['CHAMBERI', 'HORTALEZA'])
-print(viviendas_distritos(datos_filtrados))  #Devuelve un diccionario con el número total de viviendas arrendadas en Chamberí y Hortaleza.
+#datos = leer_fichero('https://datos.madrid.es/egob/catalogo/300117-0-arrendamiento-programas.csv')
+#print(distritos(datos))
+#datos_filtrados = filtrar_distritos(datos, ['CHAMBERI', 'HORTALEZA'])
+#print(viviendas_distritos(datos_filtrados))  #Devuelve un diccionario con el número total de viviendas arrendadas en Chamberí y Hortaleza.
 
