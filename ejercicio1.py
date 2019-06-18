515ce7c9cbf4088f9af914c83df6304c9a91d0c7
Paloma <p.menendez6@usp.ceu.es>
2019-06-18 13:44:56 +0200

    añadidas respuestas

--- ejercicio1.py
+++ ejercicio1.py
@@ -18,8 +18,19 @@ def codificar_palabra(palabra):
     Devuelve:
         El código morse correspondiente a la palabra con los bloques de código de cada letra separados por punto y coma.
     """
-
+    palabrabuena = palabra.replace(""," ")[1:-1]
+    palabra_codificada = ""
+    for carac in palabrabuena:
+        if carac != " " and carac.upper() in morse:
+            palabra_codificada += morse[carac.upper()]
+        elif carac == " ":
+            palabra_codificada += ";"
+    
     return palabra_codificada
+
+
+
+print(codificar_palabra('paloma'))
 
 
 def decodificar_palabra(palabra):
@@ -30,12 +41,19 @@ def decodificar_palabra(palabra):
     Devuelve:
         La palabra decodificada.
     """
+    new_dict = dict([(valor, clave) for clave, valor in morse.items()])
+    palabra_decodificada = ""
+    for carac in palabra.split(";"):
+        if carac != " " and carac.upper() in new_dict:
+            palabra_decodificada += new_dict[carac.upper()]
 
     # A partir del diccionario morse construir otro diccionario invertido, es decir, cuyas claves sean los códigos morse y cuyos valores sean las letras asociadas.
 
     # Utilizar el diccionario anterior para decodificar cada bloque de código de la palabra.
 
     return palabra_decodificada
+
+print(decodificar_palabra('paloma'))
 
 
 def codificar_mensaje(mensaje):
@@ -47,7 +65,20 @@ def codificar_mensaje(mensaje):
         El código morse correspondiente al mensaje con letras separadas por punto y coma y palabras separadas por espacio.
     """
 
+    mensajebueno= mensaje.replace(" ","$")
+    palabrabuena = mensajebueno.replace(""," ")[1:-1]
+    mensaje_codificado = ""
+    for carac in palabrabuena:
+         if carac != " " and carac.upper() in morse:
+            palabra_codificada += morse[carac.upper()]
+        elif carac == " ":
+            mensaje_codificado += ";"
+        elif carac == '$':
+            mensaje_codificado += " "
+
     return mensaje_codificado
+
+print(codificar_mensaje('hola paloma'))
 
 
 def decodificar_mensaje(palabra):
@@ -58,14 +89,20 @@ def decodificar_mensaje(palabra):
     Devuelve:
         El mensaje decodificado.
     """
+    qwe = dict([(valor, clave) for clave, valor in morse.items()]) 
+    mensaje_decodificado = ""
+    palabrabuena = palabra.split(";")
+    print(palabrabuena)
+    for carac in palabra:
+        if carac != " " and carac in qwe:
+            mensaje_decodificado += qwe[carac]
+        elif carac == " ":
+            mensaje_decodificado += " "
 
     return mensaje_decodificado
-
-
 
 # Llamada a las funciones de prueba
 print(codificar_palabra('Python')) #Debe devolver '.--.;-.--;-;....;---;-.'
 print(decodificar_palabra('.--.;-.--;-;....;---;-.')) #Debe devolver 'PYTHON'
 print(codificar_mensaje('Hola Python')) #Debe devolver '....;---;.-..;.- .--.;-.--;-;....;---;-.'
 print(decodificar_mensaje('....;---;.-..;.- .--.;-.--;-;....;---;-.')) #Debe devolver 'HOLA PYTHON'
-
